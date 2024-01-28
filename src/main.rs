b24f828facbddd9ac3d47ad8c3db1b7e49931ea8
jackycamp <jackcampanella08@gmail.com>
2024-01-28 12:37:29 -0500

    little more optimizing

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ fn read_csv(path: &str, limit: Option<usize>) -> Result<Vec<(i32, String)>, Box<
 
 
 fn main() {
-    let k = 5;
+    let k = 10;
     let train_size = 120000;
     let batch_size = 10000;
     // NOTE: original labels are 1, 2, 3, 4
@@ -62,16 +62,16 @@ fn main() {
     
 
     // let test_case = "EBay gets into rentals EBay plans to buy the apartment and home rental service Rent.com for $415 million, adding to its already exhaustive breadth of offerings.";
-    // let target_klass = 3;
-    // let test_case = "Japan’s Bonds Decline on Rate Bets, HK Stocks Gain: Markets Wrap";
-    let target_klass = 2;
-    let test_case = "Adrian Beltré, Todd Helton and Joe Mauer elected to baseball’s Hall of Fame";
-    let target_klass = 2;
-    let test_case = "Bucks fire coach Adrian Griffin after 43 games despite having one of NBA’s top records";
+    let target_klass = 3;
+    let test_case = "Japan’s Bonds Decline on Rate Bets, HK Stocks Gain: Markets Wrap";
+    // let target_klass = 2;
+    // let test_case = "Adrian Beltré, Todd Helton and Joe Mauer elected to baseball’s Hall of Fame";
+    // let target_klass = 2;
+    // let test_case = "Bucks fire coach Adrian Griffin after 43 games despite having one of NBA’s top records";
     // let target_klass = 4;
     // let test_case = "Private US lander destroyed during reentry after failed mission to moon, company says";
     println!("input text: {}", test_case);
-    println!("target class: {}", labels[target_klass - 1]);
+    println!("target class: {}, (idx: {})", labels[target_klass - 1], target_klass);
     let cx1 = compress(test_case).len();
     let train_set_shared = Arc::new(train_set);
 
@@ -96,52 +96,32 @@ fn main() {
                 let cx1x2 = compress(&combined).len();
                 let ncd = (cx1x2 as f64 - cx1.min(cx2) as f64) / cx1.max(cx2) as f64;
                 let mut dist = dist_shared_clone.lock().unwrap();
-                dist.push((ncd, train_text));
+                dist.push((idx, ncd))
             }
         });
         handles.push(handle);
     }
 
-    // for i in 0..train_size {
-    //     let dist_shared_clone = dist_shared.clone();
-    //     let train_set_shared_clone = Arc::clone(&train_set_shared);
-    //     let handle = std::thread::spawn(move || {
-    //         // let train_set_shared_clone = Arc::clone(&train_set_shared);
-    //         let train_label = train_set_shared_clone[i].0.clone();
-    //         let train_text = train_set_shared_clone[i].1.clone();
-    //
-    //         let cx2 = compress(&train_text).len();
-    //         let combined = format!("{} {}", test_case, train_text);
-    //         let cx1x2 = compress(&combined).len();
-    //         let ncd = (cx1x2 as f64 - cx1.min(cx2) as f64) / cx1.max(cx2) as f64;
-    //         let mut dist = dist_shared_clone.lock().unwrap();
-    //         dist.push((ncd, train_text));
-    //     });
-    //     handles.push(handle);
-    // }
-
     for handle in handles {
         handle.join().expect("could not join thread");
     }
 
-    let dist = dist_shared.lock().unwrap();
-
-    let mut indices_with_distances: Vec<_> = dist.iter().enumerate().collect();
-    indices_with_distances.sort_by(|&(_, dist_a), &(_, dist_b)| dist_a.partial_cmp(dist_b).unwrap());
-    let top_classes: Vec<_> = indices_with_distances.iter().take(k).map(|&(idx, _)| train_set_shared_outer[idx].0).collect();
-    println!("top classes {:?}", top_classes);
-
-    let mut counts = HashMap::new();
-    for class in &top_classes {
-        *counts.entry(class).or_insert(0) += 1;
+    let mut dist = dist_shared.lock().unwrap();
+    dist.sort_by(|a,b| a.1.partial_cmp(&b.1).unwrap());
+    let top_k_indices: Vec<usize> = dist.iter().take(k).map(|&(idx,_)|idx).collect();
+    let mut class_counts: HashMap<&i32, usize> = HashMap::new();
+    for &idx in &top_k_indices {
+        let rec = &train_set_shared_outer[idx];
+        let class = &train_set_shared_outer[idx].0;
+        *class_counts.entry(class).or_insert(0) += 1;
     }
 
-    let predict_class = counts
+    let predict_class = class_counts
         .into_iter()
-        .max_by_key(|&(_, count)| count)
-        .map(|(class, _)| *class)
-        .expect("no class found!");
-    let predict_class_idx = (predict_class - 1) as usize;
-    println!("predicted class: {}, {}", labels[predict_class_idx], predict_class);
+        .max_by(|a, b| a.1.cmp(&b.1))
+        .map(|(class, _)| class);
+    
+    let predict_class_idx = predict_class.expect("no predicted class!").to_owned() as usize;
 
+    println!("predicted class: {} (class idx {})", labels[predict_class_idx - 1], predict_class_idx);
 }
