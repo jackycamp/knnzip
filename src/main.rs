f3b71a9872f5c4c156ce0b72746246b335bd621b
jackycamp <jackcampanella08@gmail.com>
2024-01-28 17:00:09 -0500

    basic cli functionality

--- src/main.rs
+++ src/main.rs
@@ -41,38 +41,82 @@ fn read_csv(path: &str, limit: Option<usize>) -> Result<Vec<(i32, String)>, Box<
             Err(e) => println!("{}", e),
         }
     }
-    // println!("data: {:?}", data);
-    // println!("len: {:?}", data.len());
     Ok(data)
+}
+
+#[derive(Debug)]
+struct Args {
+    k: usize,
+    train_samples: i32,
+    batch_size: usize,
+    train_path: String,
+    test_path: Option<String>,
+    test_sample: String,
+    help: bool,
+}
+
+impl Args {
+    pub fn default() -> Self {
+        Self {
+            k: 10,
+            train_samples: 120000,
+            batch_size: 10000,
+            train_path: "data/ag_news/train.csv".to_string(),
+            test_path: None,
+            test_sample: "2 Magnificent Artificial Intelligence (AI) Growth Stocks Set to Join Apple and Microsoft in the $3 Trillion Club by 2030".to_string(),
+            help: false
+        }
+    }
+
+    pub fn new() -> Self {
+        let env_args: Vec<String> = std::env::args().skip(1).collect();
+        let mut args = Self::default();
+
+        for (index, argument) in env_args.iter().enumerate() {
+            if "--help" == argument || "-h" == argument {
+                args.help = true;
+            }
+            if "--train_path" == argument {
+                args.train_path = env_args[index + 1].to_string();
+            }
+            if "--test_path" == argument {
+                args.test_path = Some(env_args[index + 1].to_string());
+            }
+            if "--k" == argument {
+                args.k = env_args[index + 1].parse::<usize>().expect("cannot parse");
+            }
+            println!("Arg: {}: {}", index + 1, argument);
+        }
+        args
+    }
+
+    pub fn print_help(&self) {
+        println!("knnzip");
+        println!("usage: knnzip [OPTIONS]");
+        println!("for example: knnzip --test_sample \"Earth's Forces Are Causing This Massive Plate to Split in Two.\"");
+        println!("--train_path - Path to a training dataset (must be a csv)");
+        println!("--test_path - Path to a test dataset (must be a csv)");
+    }
 }
 
 
 fn main() {
-    let k = 10;
-    let train_size = 120000;
-    let batch_size = 10000;
+    let args = Args::new();
+    if args.help {
+        args.print_help();
+        return;
+    }
+    println!("args: {:?}", args);
     // NOTE: original labels are 1, 2, 3, 4
     let labels = ["World", "Sports", "Business", "Sci/Tech"];
     let test_set = read_csv("data/ag_news/test.csv", None).expect("cannot load test set");
-    let train_set = read_csv("data/ag_news/train.csv", Some(train_size as usize)).expect("cannot load train set");
+    let train_set = read_csv("data/ag_news/train.csv", Some(args.train_samples as usize)).expect("cannot load train set");
     println!("number samples in test set {}", &test_set.len());
     println!("number of samples in train set {}", &train_set.len());
-    println!("k: {}, train size: {}", k, train_size);
-    println!("batch size: {}", batch_size);
-    
+    println!("k: {}, num train samples: {}", args.k, args.train_samples);
+    println!("batch size: {}", args.batch_size);
 
-    // let test_case = "EBay gets into rentals EBay plans to buy the apartment and home rental service Rent.com for $415 million, adding to its already exhaustive breadth of offerings.";
-    let target_klass = 3;
-    let test_case = "Japan’s Bonds Decline on Rate Bets, HK Stocks Gain: Markets Wrap";
-    // let target_klass = 2;
-    // let test_case = "Adrian Beltré, Todd Helton and Joe Mauer elected to baseball’s Hall of Fame";
-    // let target_klass = 2;
-    // let test_case = "Bucks fire coach Adrian Griffin after 43 games despite having one of NBA’s top records";
-    // let target_klass = 4;
-    // let test_case = "Private US lander destroyed during reentry after failed mission to moon, company says";
-    println!("input text: {}", test_case);
-    println!("target class: {}, (idx: {})", labels[target_klass - 1], target_klass);
-    let cx1 = compress(test_case).len();
+    let cx1 = compress(&args.test_sample).len();
     let train_set_shared = Arc::new(train_set);
 
     let dist_shared = std::sync::Arc::new(std::sync::Mutex::new(vec![]));
@@ -80,11 +124,12 @@ fn main() {
 
     let train_set_shared_outer = Arc::clone(&train_set_shared);
 
-    for (i, batch) in train_set_shared.chunks(batch_size).enumerate() {
+    for (i, batch) in train_set_shared.chunks(args.batch_size).enumerate() {
+        let test_sample = args.test_sample.clone();
         let dist_shared_clone = Arc::clone(&dist_shared);
         let train_set_shared_clone = Arc::clone(&train_set_shared);
 
-        let start_idx = i * batch_size;
+        let start_idx = i * args.batch_size;
         let end_idx = start_idx + batch.len();
 
         let handle = std::thread::spawn(move || {
@@ -92,7 +137,7 @@ fn main() {
                 let sample = &train_set_shared_clone[idx];
                 let train_text = sample.1.clone();
                 let cx2 = compress(&train_text).len();
-                let combined = format!("{} {}", test_case, train_text);
+                let combined = format!("{} {}", test_sample, train_text);
                 let cx1x2 = compress(&combined).len();
                 let ncd = (cx1x2 as f64 - cx1.min(cx2) as f64) / cx1.max(cx2) as f64;
                 let mut dist = dist_shared_clone.lock().unwrap();
@@ -108,10 +153,9 @@ fn main() {
 
     let mut dist = dist_shared.lock().unwrap();
     dist.sort_by(|a,b| a.1.partial_cmp(&b.1).unwrap());
-    let top_k_indices: Vec<usize> = dist.iter().take(k).map(|&(idx,_)|idx).collect();
+    let top_k_indices: Vec<usize> = dist.iter().take(args.k).map(|&(idx,_)|idx).collect();
     let mut class_counts: HashMap<&i32, usize> = HashMap::new();
     for &idx in &top_k_indices {
-        let rec = &train_set_shared_outer[idx];
         let class = &train_set_shared_outer[idx].0;
         *class_counts.entry(class).or_insert(0) += 1;
     }
